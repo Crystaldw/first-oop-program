e8cd87b9d4f98e297d3223525e073240e4f73a7e
Oleksii <pogasiya@gmail.com>
2023-10-05 18:33:46 +0200

    update

---
+++ first-oop-program/src/somewhere/SumThreeNumbers.java
@@ -0,0 +1,12 @@
+package somewhere;
+
+public class SumThreeNumbers {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=1;
+        b=2;
+        c=3;
+        int result = a+b+c;
+        System.out.println(result);
+    }
+}
--- first-oop-program/src/somewhere/XXX.java
+++
@@ -1,8 +0,0 @@
-package somewhere;
-
-public class XXX {
-    public static void main(String[] args) {
-        int x = 1+7*2;
-        System.out.println(x);
-    }
-}
