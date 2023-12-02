f2044846a8816cb157245da37eb2dbc17e64ced8
Oleksii <pogasiya@gmail.com>
2023-12-02 14:18:26 +0100

    commit

---
+++ hwspring1/src/main/java/de/telran/hwspring1/Hwspring1Application.java
@@ -0,0 +1,13 @@
+package de.telran.hwspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwspring1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hwspring1Application.class, args);
+    }
+
+}
