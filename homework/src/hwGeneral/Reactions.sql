cf6a6bd035dfed04eb35cfedbeaff6b8a2ea04ce
Oleksii Pohasii <131201402+Crystaldw@users.noreply.github.com>
2023-12-12 17:40:40 +0100

    Add files via upload
    commit

---
+++ homework/src/hwGeneral/Reactions.sql
@@ -0,0 +1,19 @@
+//Создать таблицы реакций и комментариев
+
+//Добавить несколько записей в каждую таблицу выше
+
+
+CREATE TABLE reactions (
+		reaction_id int,
+    created_at timestamp,
+    value int,
+		author_id int,
+    video_id int
+);
+
+INSERT INTO reactions (reaction_id, created_at, value, author_id, video_id)
+VALUES
+	(1, CURRENT_TIMESTAMP, 5, 1, 1),
+	(2, CURRENT_TIMESTAMP, 5, 4, 2),
+	(3, CURRENT_TIMESTAMP, 5, 2, 1),
+	(4, CURRENT_TIMESTAMP, 5, 5, 2)
